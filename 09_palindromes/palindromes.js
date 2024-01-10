ab899ae323d7d1b4139bf4e688b0c584a6a41dc0
Eoin-k <100050380+Eoin-k@users.noreply.github.com01~>
2024-01-10 01:40:53 +0000

    Completed exercises 9 - 12

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,8 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let lowerCase = string.toLowerCase().replace(/[^a-z0-9]/g, ""); 
+    return lowerCase.split("").reverse().join("") == lowerCase;
+}
 
-};
 
 // Do not edit below this line
 module.exports = palindromes;
--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,18 @@
-const getTheTitles = function() {
+const books = [
+    {
+      title: 'Book 1',
+      author: 'Author 1'
+    },
+    {
+      title: 'Book2',
+      author: 'Author 2'
+    }
+  ]
+
+const getTheTitles = function(books) {
+    return books.map((books => books.title))
+    
 
 };
-
 // Do not edit below this line
 module.exports = getTheTitles;
